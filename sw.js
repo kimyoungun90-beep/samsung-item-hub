70e93500459dbcf2080bc25c6e6d0fb14b6552fa
kimyoungun90-beep <kim.youngun90@gmail.com>
2026-06-03 17:18:12 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'costco-item-operation-hub-v47';
+const CACHE_NAME = 'costco-item-operation-hub-v48';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './tv_wall_install_fee.png'];
 
 self.addEventListener('install', event => {
