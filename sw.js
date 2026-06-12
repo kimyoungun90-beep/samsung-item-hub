7b78b1d1fbb944dde950331c05c4fe20136b10b7
kimyoungun90-beep <kim.youngun90@gmail.com>
2026-06-12 21:27:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'costco-item-operation-hub-v73';
+const CACHE_NAME = 'costco-item-operation-hub-v74';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './tv_wall_install_fee.png'];
 
 self.addEventListener('install', event => {
